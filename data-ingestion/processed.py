3df709b215ee678a4b3af3072aa3ca67e23af87a
lembzg <107914643+lembzg@users.noreply.github.com>
2026-02-02 22:58:03 +0000

    Modify parquet file path to an empty string
    Updated the file path for reading parquet data.

--- data-ingestion/processed.py
+++ data-ingestion/processed.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_parquet('/home/j39233pt/Desktop/Nebula_Apex_MM/data-ingestion/data/1_BTC-USDT_28-05-25_12-48-30.parquet', engine='pyarrow')
+df = pd.read_parquet('', engine='pyarrow')
 
 print(df.head())
